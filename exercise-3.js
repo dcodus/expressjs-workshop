dd3fab087e247902397a19e60f1648cc7701e15e
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-30 20:01:49 +0000

    Exercise #: 3 Updated solution

--- exercise-3.js
+++ exercise-3.js
@@ -1,27 +1,41 @@
 var express = require("express");
 var app = express();
 
+function calculate(operation, num1, num2) {
+    var answer = {
+        operation: operation,
+        firstOperand: num1,
+        secondOperand: num2,
+    };
+
+    if (operation === 'add') {
+        answer.solution = (num1) + (num2);
+    }
+    else if (operation === 'sub') {
+        answer.solution = (num1) - (num2);
+    }
+    else if (operation === 'mult') {
+        answer.solution = (num1) * (num2);
+    }
+    else if (operation === 'div') {
+        answer.solution = (num1) / (num2);
+    }
+    return answer;
+}
+
+
+
 
 app.get('/op/:operation/:number1/:number2', function(req, res) {
 
-    var answer;
+ 
+ if(req.params.operation === 'add' || req.params.operation === 'sub' || req.params.operation === 'mult' || req.params.operation === 'div'){
+      var result = calculate(req.params.operation, Number(req.params.number1), Number(req.params.number2));
+      res.json(result);
+ } else {
+     res.status(404).send();
+ }
 
-    if (req.params.operation === 'add') {
-        answer = Number(req.params.number1) + Number(req.params.number2);
-        res.send(answer.toString());
-    }
-    else if (req.params.operation === 'sub') {
-        answer = Number(req.params.number1) - Number(req.params.number2);
-        res.send(answer.toString());
-    }
-    else if (req.params.operation === 'mult') {
-        answer = Number(req.params.number1) * Number(req.params.number2);
-        res.send(answer.toString());
-    }
-    else if (req.params.operation === 'div') {
-        answer = Number(req.params.number1) / Number(req.params.number2);
-        res.send(answer.toString());
-    }
 
 })
 
